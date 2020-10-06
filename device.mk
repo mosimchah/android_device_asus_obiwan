a5428bfb338636dd2122629cd8ad00a091821ca4
mosimchah <mosimchah@gmail.com>
2020-10-05 21:40:39 -0700

    AICP-ify
    Change-Id: I800b5fb462f4f2097fe62680a7ee063976f12c99

--- device.mk
+++ device.mk
@@ -54,7 +54,7 @@ PRODUCT_COPY_FILES += \
 
 # Lineage Permissions
 PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    vendor/aicp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # Touch
 PRODUCT_PACKAGES += \
